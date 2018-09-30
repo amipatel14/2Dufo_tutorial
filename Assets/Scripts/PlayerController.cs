212bad673a38cb7159b2f1ffbbc284538ec47ac3
amipatel14 <32078080+amipatel14@users.noreply.github.com>
2018-09-30 18:53:40 -0500

    Step Eight
    Counting Collectables and Displaying Score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float Speed;
+    public Text CountText;
+    public Text WinText;
     private Rigidbody2D rb2D;
+    private int count;
+    
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+        WinText.text = " ";
     }
     void FixedUpdate()
     {
@@ -18,14 +26,24 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2D.AddForce(movement*speed);
+        rb2D.AddForce(movement*Speed);
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        CountText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            WinText.text = "YOU WIN!";
         }
     }
 }
